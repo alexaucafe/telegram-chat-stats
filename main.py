dcbbc9be5badb21674fb3029a90ac073a04ad164
Alex <112026581+alexaucafe@users.noreply.github.com>
2023-01-25 22:03:11 +0300

    Added txt export

--- main.py
+++ main.py
@@ -5,26 +5,38 @@ def main():
     group = Chat()
     group_stats = group.get_group_stats()
 
-    print(txt(group_stats))
+    text = txt(group_stats)
+    print(text)
+
+    with open("result.txt", "w", encoding = "utf-8") as file:
+        file.write(text)
+        file.close()
 
 def txt(group_stats, first_names = True):
 
     text_lines: list = []
 
-    longest_first_name = max(group_stats["members"], key = lambda member: len(member["first_name"]))
-    longest_name_len = len(longest_first_name["first_name"])
+    if first_names == True:
+        longest_name_member = max(group_stats["members"], key = lambda member: len(member["first_name"]))
+        longest_name_len = len(longest_name_member["first_name"])
+    else:
+        longest_name_member = max(group_stats["members"], key = lambda member: len(member["name"]))
+        longest_name_len = len(longest_name_member["name"])
 
     spaces = " "*(longest_name_len + 2)
     text_lines.append("{}{}\n".format(spaces, group_stats["name"]))
 
     for member in group_stats["members"]:
-        first_name = member["first_name"]
+        if first_names == True:
+            name = member["first_name"]
+        else:
+            name = member["name"]
         messages_count = member["messages_count"]
         forwards_count = member["forwards_count"]
         replies_count = member["replies_count"]
 
-        spaces = " "*(longest_name_len - len(first_name))
-        text_lines.append(f"{first_name}:{spaces} {messages_count} messages ({replies_count} replies, {forwards_count} forwarded)")
+        spaces = " "*(longest_name_len - len(name))
+        text_lines.append(f"{name}:{spaces} {messages_count} messages ({replies_count} replies, {forwards_count} forwarded)")
     
     spaces = " "*(longest_name_len + 2)
     text_lines.append(f"\n{spaces}309:Дата-отдел")
