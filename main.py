7bd4a80efb1ebb8394c41e3de1cbf0a635f62056
Alex <112026581+alexaucafe@users.noreply.github.com>
2023-01-25 21:49:34 +0300

    Moved text export to separate function

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ def main():
     group = Chat()
     group_stats = group.get_group_stats()
 
+    print(txt(group_stats))
+
+def txt(group_stats, first_names = True):
+
+    text_lines: list = []
+
     longest_first_name = max(group_stats["members"], key = lambda member: len(member["first_name"]))
     longest_name_len = len(longest_first_name["first_name"])
 
     spaces = " "*(longest_name_len + 2)
-    print("{}{}\n".format(spaces, group_stats["name"]))
+    text_lines.append("{}{}\n".format(spaces, group_stats["name"]))
 
     for member in group_stats["members"]:
         first_name = member["first_name"]
@@ -18,9 +24,11 @@ def main():
         replies_count = member["replies_count"]
 
         spaces = " "*(longest_name_len - len(first_name))
-        print(f"{first_name}:{spaces} {messages_count} messages ({replies_count} replies, {forwards_count} forwarded)")
+        text_lines.append(f"{first_name}:{spaces} {messages_count} messages ({replies_count} replies, {forwards_count} forwarded)")
     
     spaces = " "*(longest_name_len + 2)
-    print(f"\n{spaces}309:Дата-отдел")
+    text_lines.append(f"\n{spaces}309:Дата-отдел")
+
+    return "\n".join(text_lines)
 
 main()
